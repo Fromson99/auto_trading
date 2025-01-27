c8656214d1aa0e35d6a291ed6a9b7c5f42b5f27a
DESKTOP-01282LV\son <fromson99@gmail.com>
2025-01-27 23:44:50 +0900

    no message

---
+++ __init__.py
@@ -0,0 +1,13 @@
+from kiwoom.kiwoom import *
+import sys
+from PyQt5.QtWidgets import *
+
+class Main():
+    def __init__(self):
+        print("Main() start")
+
+        self.app = QApplication(sys.argv) #PyQt5로 실행할 파일명을 자동 설정
+        self.kiwoom = Kiwoom()
+        self.app.exec_()
+if __name__ == "__main__":
+    Main()
